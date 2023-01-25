5f8192c50d51bcf71ff196b7e4b3e2c950073fb2
uwiragiyem <117993530+uwiragiyem@users.noreply.github.com>
2023-01-25 14:02:24 -0800

    AIRLINE TICKECT
    management system

---
+++ src/javasql/Javasql.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javasql;
+
+/**
+ *
+ * @author ERIC
+ */
+public class Javasql {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginForm log = new LoginForm();
+        log.show();
+    }
+    
+}
